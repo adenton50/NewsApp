07f6ef5388c26e08ec7ebbbebdc5e64cbc34f950
adam denton <adamdenton50@gmail.com>
2024-08-16 18:11:56 -0500

    update two

--- newsapp/src/components/Card.jsx
+++ newsapp/src/components/Card.jsx
@@ -1,7 +1,7 @@
 import CardItem from "./CardItem";
 
 export default function Card({ data, query }) {
-  console.log(data);
+  //console.log(data);
   return (
     <div className="grid grid-cols-4 gap-24 p-16 m-8">
       {data.map((article) => (
